6a3f132a07a508f2fea24287223c2b52f90fc2ed
Nicholas-Vignogna <nick.vignogna@gmail.com>
2023-07-11 16:27:34 -0400

    completed google sign in / home page

--- app/src/main/java/com/example/iron_deers_query_theory/MainActivity.java
+++ app/src/main/java/com/example/iron_deers_query_theory/MainActivity.java
@@ -2,13 +2,95 @@ package com.example.iron_deers_query_theory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    GoogleSignInOptions gso;
+
+    GoogleSignInClient gsc;
+
+    ImageView GoogleBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        GoogleBtn = findViewById(R.id.GoogleBTN);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this,gso);
+
+        GoogleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+
+        TextView username = findViewById(R.id.username);
+        TextView password = findViewById(R.id.password);
+
+        MaterialButton LoginIn = (MaterialButton) findViewById(R.id.LogIn);
+
+        //admin account info
+
+
+        LoginIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin"))
+                {
+                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
+
+
+
+    void signIn()
+    {
+        Intent signInIntent = gsc.getSignInIntent();
+        startActivityForResult(signInIntent, 1000);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
