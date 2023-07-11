cd1eb8fbe777e8ba5dc4bce0696ae176d1fa0cba
Emmy2345 <123962727+Emmy2345@users.noreply.github.com>
2023-07-11 19:50:39 -0400

    Completed Buttons for Home Screen.

--- app/src/main/java/com/example/iron_deers_query_theory/StartScreen.java
+++ app/src/main/java/com/example/iron_deers_query_theory/StartScreen.java
@@ -2,13 +2,23 @@ package com.example.iron_deers_query_theory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class StartScreen extends AppCompatActivity {
-
+    Button start_Learning;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_screen);
+
+        start_Learning = findViewById(R.id.start_Going);
+        start_Learning.setOnClickListener(v -> {
+
+         Intent intent = new Intent(StartScreen.this, MainActivity.class);
+         startActivity(intent);
+        });
     }
 }
