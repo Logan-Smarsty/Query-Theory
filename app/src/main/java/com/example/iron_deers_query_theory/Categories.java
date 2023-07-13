6e8e51b3393918d6620a74aa18b64a35673ee119
Emmy2345 <123962727+Emmy2345@users.noreply.github.com>
2023-07-13 16:06:44 -0400

    Button Math working.

--- app/src/main/java/com/example/iron_deers_query_theory/Categories.java
+++ app/src/main/java/com/example/iron_deers_query_theory/Categories.java
@@ -5,19 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.Rect;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 public class Categories extends AppCompatActivity {
 
     Button Math;
-    Button Science;
-    Button English;
-    Button History;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,29 +28,6 @@ public class Categories extends AppCompatActivity {
             startActivity(intent);
         });
 
-        Science = findViewById(R.id.CatScience);
-        Science.setVisibility(View.VISIBLE);
-        Science.setBackgroundColor(Color.TRANSPARENT);
 
-        Science.setOnClickListener(v -> {
-            Intent intent = new Intent(Categories.this, ScienceQuestion.class);
-            startActivity(intent);
-        });
-
-        English = findViewById(R.id.CatEnglish);
-        English.setVisibility(View.VISIBLE);
-        English.setBackgroundColor(Color.TRANSPARENT);
-        English.setOnClickListener(v -> {
-            Intent intent = new Intent(Categories.this, EnglishQuestion.class);
-            startActivity(intent);
-        });
-
-        History = findViewById(R.id.CatHistory);
-        History.setVisibility(View.VISIBLE);
-        History.setBackgroundColor(Color.TRANSPARENT);
-        History.setOnClickListener(v -> {
-            Intent intent = new Intent(Categories.this, HistoryQuestion.class);
-            startActivity(intent);
-        });
     }
 }
