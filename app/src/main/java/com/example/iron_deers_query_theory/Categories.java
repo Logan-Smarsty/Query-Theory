cd7d9a51ccd4d77fa2749116718a3a85fefca210
Emmy2345 <123962727+Emmy2345@users.noreply.github.com>
2023-07-13 15:43:29 -0400

    deleted QT from log in

--- app/src/main/java/com/example/iron_deers_query_theory/Categories.java
+++ app/src/main/java/com/example/iron_deers_query_theory/Categories.java
@@ -4,14 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 
 public class Categories extends AppCompatActivity {
 
-    Button Math;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +17,6 @@ public class Categories extends AppCompatActivity {
         setContentView(R.layout.categories);
 
 
-        Math = findViewById(R.id.CatMath);
-        Math.setVisibility(View.VISIBLE);
-        Math.setBackgroundColor(Color.TRANSPARENT);
-
-        Math.setOnClickListener(v -> {
-            Intent intent = new Intent(Categories.this, MathQuestion.class);
-            startActivity(intent);
-        });
 
 
     }
