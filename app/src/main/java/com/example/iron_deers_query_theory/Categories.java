008ffcc4c1ebab18e44a4f41c15d0d5048f23e8c
Emmy2345 <123962727+Emmy2345@users.noreply.github.com>
2023-07-13 16:06:44 -0400

    Button Math working.

--- app/src/main/java/com/example/iron_deers_query_theory/Categories.java
+++ app/src/main/java/com/example/iron_deers_query_theory/Categories.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
 
 public class Categories extends AppCompatActivity {
 
-
+    Button Math;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,14 @@ public class Categories extends AppCompatActivity {
         setContentView(R.layout.categories);
 
 
+        Math = findViewById(R.id.CatMath);
+        Math.setVisibility(View.VISIBLE);
+        Math.setBackgroundColor(Color.TRANSPARENT);
+
+        Math.setOnClickListener(v -> {
+            Intent intent = new Intent(Categories.this, MathQuestion.class);
+            startActivity(intent);
+        });
 
 
     }
