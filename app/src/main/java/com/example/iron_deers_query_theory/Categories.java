af4fec5d09c699f8e6c20e67be09068a3a50c803
Emmy2345 <123962727+Emmy2345@users.noreply.github.com>
2023-07-13 19:19:52 -0400

    Added the button commands for other windows and menu bar.

--- app/src/main/java/com/example/iron_deers_query_theory/Categories.java
+++ app/src/main/java/com/example/iron_deers_query_theory/Categories.java
@@ -15,7 +15,9 @@ import android.widget.ImageView;
 public class Categories extends AppCompatActivity {
 
     Button Math;
-    ImageView CatIcon;
+    Button Science;
+    Button English;
+    Button History;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +31,31 @@ public class Categories extends AppCompatActivity {
 
         Math.setOnClickListener(v -> {
             Intent intent = new Intent(Categories.this, MathQuestion.class);
+            startActivity(intent);
+        });
+
+        Science = findViewById(R.id.CatScience);
+        Science.setVisibility(View.VISIBLE);
+        Science.setBackgroundColor(Color.TRANSPARENT);
+
+        Science.setOnClickListener(v -> {
+            Intent intent = new Intent(Categories.this, ScienceQuestion.class);
+            startActivity(intent);
+        });
+
+        English = findViewById(R.id.CatEnglish);
+        English.setVisibility(View.VISIBLE);
+        English.setBackgroundColor(Color.TRANSPARENT);
+        English.setOnClickListener(v -> {
+            Intent intent = new Intent(Categories.this, EnglishQuestion.class);
+            startActivity(intent);
+        });
+
+        History = findViewById(R.id.CatHistory);
+        History.setVisibility(View.VISIBLE);
+        History.setBackgroundColor(Color.TRANSPARENT);
+        History.setOnClickListener(v -> {
+            Intent intent = new Intent(Categories.this, HistoryQuestion.class);
             startActivity(intent);
         });
     }
