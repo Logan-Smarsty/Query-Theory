9eb35d0c44c14d03492d5b823dc2a169afab4075
Emmy2345 <123962727+Emmy2345@users.noreply.github.com>
2023-07-13 19:19:52 -0400

    Added the button commands for other windows and menu bar.

--- app/src/main/java/com/example/iron_deers_query_theory/HistoryQuestion.java
+++ app/src/main/java/com/example/iron_deers_query_theory/HistoryQuestion.java
@@ -2,13 +2,25 @@ package com.example.iron_deers_query_theory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class HistoryQuestion extends AppCompatActivity {
 
+    ImageView CatIcon;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.history_question);
+
+        CatIcon = findViewById(R.id.Cat);
+        CatIcon.setOnClickListener(v -> {
+            Intent intent = new Intent(HistoryQuestion.this, Categories.class);
+            startActivity(intent);
+        });
     }
 }
