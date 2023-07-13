7c10971b1b61b4ca5ca6249685304eda4bf4a391
Emmy2345 <123962727+Emmy2345@users.noreply.github.com>
2023-07-13 18:43:30 -0400

    Added menu bAR TO Math screen.

--- app/src/main/java/com/example/iron_deers_query_theory/MathQuestion.java
+++ app/src/main/java/com/example/iron_deers_query_theory/MathQuestion.java
@@ -2,13 +2,25 @@ package com.example.iron_deers_query_theory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MathQuestion extends AppCompatActivity {
 
+    ImageView CatIcon;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.math_question);
+
+        CatIcon = findViewById(R.id.Cat);
+        CatIcon.setOnClickListener(v -> {
+            Intent intent = new Intent(MathQuestion.this, Categories.class);
+            startActivity(intent);
+        });
     }
+
 }
