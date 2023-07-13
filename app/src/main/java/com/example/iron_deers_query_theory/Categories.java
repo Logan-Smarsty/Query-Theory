d3377fe0ff21225fc068cf43320622346b25ac4b
Emmy2345 <123962727+Emmy2345@users.noreply.github.com>
2023-07-13 18:36:10 -0400

    Fixed Categories

--- app/src/main/java/com/example/iron_deers_query_theory/Categories.java
+++ app/src/main/java/com/example/iron_deers_query_theory/Categories.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Categories extends AppCompatActivity {
 
     Button Math;
+    ImageView CatIcon;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +31,5 @@ public class Categories extends AppCompatActivity {
             Intent intent = new Intent(Categories.this, MathQuestion.class);
             startActivity(intent);
         });
-
-
     }
 }
