08f2bcadc73b688dd370e02d8a0fe7f31754ffec
Emmy2345 <123962727+Emmy2345@users.noreply.github.com>
2023-07-13 15:43:29 -0400

    deleted QT from log in

--- app/src/main/java/com/example/iron_deers_query_theory/Categories.java
+++ app/src/main/java/com/example/iron_deers_query_theory/Categories.java
@@ -2,13 +2,22 @@ package com.example.iron_deers_query_theory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Categories extends AppCompatActivity {
 
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.categories);
+
+
+
+
     }
 }
