7e69241dc4c3d8a5b93eac9b2177f3ce34172c1f
Emmy2345 <123962727+Emmy2345@users.noreply.github.com>
2023-07-13 20:17:20 -0400

    Erase Button word in Category window.

--- app/src/main/java/com/example/iron_deers_query_theory/Categories.java
+++ app/src/main/java/com/example/iron_deers_query_theory/Categories.java
@@ -5,14 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Categories extends AppCompatActivity {
 
     Button Math;
-    @SuppressLint("MissingInflatedId")
+    Button Science;
+    Button English;
+    Button History;
+    @SuppressLint({"MissingInflatedId", "UseCompatLoadingForDrawables"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +33,32 @@ public class Categories extends AppCompatActivity {
             Intent intent = new Intent(Categories.this, MathQuestion.class);
             startActivity(intent);
         });
+
+        Science = findViewById(R.id.CatScience);
+        Science.setVisibility(View.VISIBLE);
+        Science.setBackgroundColor(Color.TRANSPARENT);
+
+        Science.setOnClickListener(v -> {
+            Intent intent = new Intent(Categories.this, ScienceQuestion.class);
+            startActivity(intent);
+        });
+
+        English = findViewById(R.id.CatEnglish);
+        English.setVisibility(View.VISIBLE);
+        English.setBackgroundColor(Color.TRANSPARENT);
+        English.setOnClickListener(v -> {
+            Intent intent = new Intent(Categories.this, EnglishQuestion.class);
+            startActivity(intent);
+        });
+
+        History = findViewById(R.id.CatHistory);
+        History.setVisibility(View.VISIBLE);
+        History.setBackgroundColor(Color.TRANSPARENT);
+        History.setOnClickListener(v -> {
+            Intent intent = new Intent(Categories.this, HistoryQuestion.class);
+            startActivity(intent);
+        });
+        final ImageView image = (ImageView) findViewById(R.id.CatScreen);
 
 
     }
