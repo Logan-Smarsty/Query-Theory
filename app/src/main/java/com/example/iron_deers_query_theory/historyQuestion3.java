2c61b53474d07e8a2df234b10fb01ea5b6c99bbc
KMarly D <44208106+Kemarly@users.noreply.github.com>
2023-08-08 16:34:04 -0400

    history questions done

---
+++ app/src/main/java/com/example/iron_deers_query_theory/historyQuestion3.java
@@ -0,0 +1,14 @@
+package com.example.iron_deers_query_theory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class historyQuestion3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.history_question3);
+    }
+}
