2d5cbaa9565a5097581d6a5a6dafad6d4f0b8da9
KMarly D <44208106+Kemarly@users.noreply.github.com>
2023-08-24 16:20:53 -0400

    all warnings gone

--- app/src/main/java/com/example/iron_deers_query_theory/Fragments/CategoryFragment.java
+++ app/src/main/java/com/example/iron_deers_query_theory/Fragments/CategoryFragment.java
@@ -9,8 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridView;
-import android.widget.ImageView;
+//import android.widget.GridView;
+//import android.widget.ImageView;
 
 import com.example.iron_deers_query_theory.Activities.EnglishQuestion;
 import com.example.iron_deers_query_theory.Activities.HistoryQuestion;
@@ -18,7 +18,7 @@ import com.example.iron_deers_query_theory.Activities.MathQuestion;
 import com.example.iron_deers_query_theory.Activities.ScienceQuestion;
 import com.example.iron_deers_query_theory.R;
 
-/**
+/*
  * A simple {@link Fragment} subclass.
  * Use the {@link CategoryFragment#newInstance} factory method to
  * create an instance of this fragment.
@@ -27,20 +27,13 @@ public class CategoryFragment extends Fragment
 {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private ImageView English, Science, Math, History;
     public CategoryFragment()
     {
         // Required empty public constructor
     }
 
-    /**
+    /*
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
@@ -49,25 +42,21 @@ public class CategoryFragment extends Fragment
      * @return A new instance of fragment CategoryFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CategoryFragment newInstance(String param1, String param2)
-    {
-        CategoryFragment fragment = new CategoryFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    //public static CategoryFragment newInstance(String param1, String param2)
+    //{
+        //CategoryFragment fragment = new CategoryFragment();
+        //Bundle args = new Bundle();
+        //args.putString(ARG_PARAM1, param1);
+        //args.putString(ARG_PARAM2, param2);
+        //fragment.setArguments(args);
+        //return fragment;
+    //}
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null)
-        {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        //if (getArguments() != null) {}
     }
 
     @Override
@@ -81,41 +70,21 @@ public class CategoryFragment extends Fragment
         CardView English = view.findViewById(R.id.english_cardview);
         CardView Science = view.findViewById(R.id.science_cardview);
 
-        English.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-               Intent intent = new Intent(getActivity(), EnglishQuestion.class);
-               startActivity(intent);
-            }
+        English.setOnClickListener(v -> {
+           Intent intent = new Intent(getActivity(), EnglishQuestion.class);
+           startActivity(intent);
         });
-        Math.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(getActivity(), MathQuestion.class);
-                startActivity(intent);
-            }
+        Math.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), MathQuestion.class);
+            startActivity(intent);
         });
-        Science.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(getActivity(), ScienceQuestion.class);
-                startActivity(intent);
-            }
+        Science.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), ScienceQuestion.class);
+            startActivity(intent);
         });
-        History.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(getActivity(), HistoryQuestion.class);
-                startActivity(intent);
-            }
+        History.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), HistoryQuestion.class);
+            startActivity(intent);
         });
         return view;
     }
--- app/src/main/java/com/example/iron_deers_query_theory/Fragments/SettingsFragment.java
+++ app/src/main/java/com/example/iron_deers_query_theory/Fragments/SettingsFragment.java
@@ -10,7 +10,7 @@ import android.view.ViewGroup;
 
 import com.example.iron_deers_query_theory.R;
 
-/**
+/*
  * A simple {@link Fragment} subclass.
  * Use the {@link SettingsFragment#newInstance} factory method to
  * create an instance of this fragment.
@@ -19,15 +19,15 @@ public class SettingsFragment extends Fragment
 {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    //private static final String ARG_PARAM1 = "param1";
+    //private static final String ARG_PARAM2 = "param2";
 
     public SettingsFragment()
     {
         // Required empty public constructor
     }
 
-    /**
+    /*
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
@@ -36,25 +36,25 @@ public class SettingsFragment extends Fragment
      * @return A new instance of fragment SettingsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2)
-    {
-        SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+   //public static SettingsFragment newInstance(String param1, String param2)
+    //{
+        //SettingsFragment fragment = new SettingsFragment();
+        //Bundle args = new Bundle();
+        //args.putString(ARG_PARAM1, param1);
+        //args.putString(ARG_PARAM2, param2);
+        //fragment.setArguments(args);
+        //return fragment;
+    //}
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null)
+        //if (getArguments() != null)
         {
             // TODO: Rename and change types of parameters
-            String mParam1 = getArguments().getString(ARG_PARAM1);
-            String mParam2 = getArguments().getString(ARG_PARAM2);
+            //String mParam1 = getArguments().getString(ARG_PARAM1);
+            //String mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
