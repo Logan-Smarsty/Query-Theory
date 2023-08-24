2c250af9f1c2c3bf5cabd2212b06fa386f3cff68
KMarly D <44208106+Kemarly@users.noreply.github.com>
2023-08-24 17:57:31 -0400

    text view

--- app/src/main/java/com/example/iron_deers_query_theory/Activities/MathQuestion.java
+++ app/src/main/java/com/example/iron_deers_query_theory/Activities/MathQuestion.java
@@ -227,7 +227,7 @@ public class MathQuestion extends AppCompatActivity
             score ++;
             selectedOption.setBackgroundResource(R.drawable.back_color_green);
             //Code to Display the Current Score
-            TextView cat = (TextView)findViewById(R.id.scores);
+            TextView cat = findViewById(R.id.scores);
             cat.setText("Score:" + score  + "/10");
         }
         else
